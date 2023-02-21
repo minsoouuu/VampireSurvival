be5ec1bf03056a7c6ec6cff66133c77129137b5c
dbsrjs <dbsrjs1224@gmail.com>
2023-02-21 21:50:51 +0900

    123

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
+    [SerializeField] private GameObject player;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector3.Lerp(transform.position, player.transform.position, 2f *Time.deltaTime);
+
     }
 }
--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,34 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class MapController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    [SerializeField] List<GameObject> maps;
-    [SerializeField] List<GameObject> collides;
+    [SerializeField] private List<GameObject> maps;
+
+    float playerXpos;
+    float playerYpos;
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (player.transform.position.x > 10)
+        if (player.transform.position.x % 10 == 0)
         {
-            maps[0].transform.position = new Vector3(30, 10, 0);
-            maps[2].transform.position = new Vector3(30, -10, 0);
+            if (player.transform.position.x > 10)
+            {
+                maps[0].transform.position = new Vector3(30, 10, 0);
+                maps[2].transform.position = new Vector3(30, -10, 0);
+            }
+            else if (player.transform.position.x < -10)
+            {
+                maps[1].transform.position = new Vector3(30, 10, 0);
+                maps[3].transform.position = new Vector3(30, -10, 0);
+            }
         }
-        else if (player.transform.position.x < -10)
+        else if (player.transform.position.y % 10 == 0)
         {
-            maps[0].transform.position = new Vector3(-30, 10, 0);
-            maps[2].transform.position = new Vector3(-30, -10, 0);
+            if (player.transform.position.y > 10)
+            {
+                maps[2].transform.position = new Vector3();
+                maps[3].transform.position = new Vector3();
+            }
+            else if (player.transform.position.y < -10)
+            {
+                maps[0].transform.position = new Vector3();
+                maps[1].transform.position = new Vector3();
+            }
         }
     }
 }
